d901a8dae52c086931756f468078d684aeb81a1d
huynet <cocwedc@gmail.com>
2019-12-05 16:35:06 -0600

    a bunch of migrations;

--- db/migrate/20191205035322_do_nothing_yet.rb
+++ db/migrate/20191205035322_do_nothing_yet.rb
@@ -1,8 +1,10 @@
 class DoNothingYet < ActiveRecord::Migration[6.0]
   def up
+    puts "*** It all begins"
   end
 
   def down
+    puts "Back to square 1"
   end
   
 end
---
+++ db/migrate/20191205213739_alter_users.rb
@@ -0,0 +1,24 @@
+class AlterUsers < ActiveRecord::Migration[6.0]
+  def up
+    rename_table("users", "admin_users")
+    add_column("admin_users", "username", :string, :limit => 25, :after => "email")
+    change_column("admin_users", "email", :string, :limit => 100)
+    puts "*** Start renaming column"
+    rename_column("admin_users", "password", "hashed_password")
+    puts "End renaming column"
+
+    puts "*** Start adding an index ***"
+    add_index("admin_users", "username")
+    puts "*** Finish adding an index ***"
+  end
+
+  def down
+    puts "*** Start removing an index ***"
+    remove_index("admin_users", "username")
+    puts "*** Finish removing an index ***"
+    rename_column("admin_users", "hashed_password", "password")
+    change_column("admin_users", "email", :string, :default => '', :null => false)
+    remove_column("admin_users", "username")
+    rename_table("admin_users", "users")
+  end
+end
