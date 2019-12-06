fdd590e535c69f0bdc896f395ecc9eb265d7d046
huynet <cocwedc@gmail.com>
2019-12-06 16:59:16 -0600

    user => admin_user

---
+++ app/models/admin_user.rb
@@ -0,0 +1,11 @@
+# First method to change the name 
+
+# class User < ApplicationRecord
+#   self.table_name = "admin_users"
+# end
+
+# Second method to change the name
+
+class AdminUser < ApplicationRecord
+
+end
