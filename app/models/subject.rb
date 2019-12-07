df4d5f5602a0d40dbe834a38ef70e40e68fb6e6d
huynet <cocwedc@gmail.com>
2019-12-06 18:07:34 -0600

    querying and scoping

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,14 @@
 class Subject < ApplicationRecord
+
+  scope :visible, lambda { where :visible => true }
+  scope :invisible, lambda { where(:visible => false )}
+
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { order :created_at => :desc }
+  # partial match works with LIKE
+  scope :search, lambda { |query|
+    where(["name LIKE ?", "%#{query}%"])
+  }
+
+
 end
